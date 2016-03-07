957341a433545dc4c80f60be5f6122f7f366df49
Ivan Moroz <genom-1990@yandex.ru>
2016-03-07 17:03:44 +0300

    Оптимизировали множественную запись в базу данных

--- app/models/date_event.rb
+++ app/models/date_event.rb
@@ -22,11 +22,12 @@ class DateEvent < ActiveRecord::Base
 
     if type_event == 'not_repeat' && !(last_date_point.present?)
       return array_records
-    else
-      if  record.date_start < last_date_point
-        return nil
-      end
     end
+
+    if  last_date_point.present? && record.date_start < last_date_point
+      return nil
+    end
+
 
     template_record = record.attributes.dup
     new_date_start = template_record["date_start"]
@@ -50,7 +51,20 @@ class DateEvent < ActiveRecord::Base
   def self.generate_dates_event(event, date_start)
     date_event = event.date_events.build(date_start: date_start)
     new_rows_hash = DateEvent.get_collection_event_date(date_event)
-    DateEvent.create(new_rows_hash) if new_rows_hash.present?
+
+    #Fast
+    DateEvent.single_mass_insert_sql!(new_rows_hash) if new_rows_hash.present?
+
+  end
+
+  def self.single_mass_insert_sql!(rows)
+    inserts = []
+    rows.each  do |row|
+      inserts.push "(#{row["event_id"]},\'#{row["date_start"]}\')"
+    end
+    sql = "INSERT INTO date_events (event_id, date_start) VALUES #{inserts.join(", ")}"
+    connection = ActiveRecord::Base.connection
+    connection.execute(sql)
   end
 
   def start_time
