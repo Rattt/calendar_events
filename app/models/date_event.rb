53df6d577b0dc8784069a71d1bb1ee4074dab3c9
Ivan Moroz <genom-1990@yandex.ru>
2016-03-08 11:23:41 +0300

    Задаем диапазоны хранения временных меток и генерации, избавляемся от магических чисел.

--- app/models/date_event.rb
+++ app/models/date_event.rb
@@ -5,16 +5,15 @@ class DateEvent < ActiveRecord::Base
   scope :get_date_last_point, -> {order(date_start: :desc).limit(1).pluck(:date_start).first }
   scope :get_date_start_first_event_occurs, -> { where("DATE(date_start) > ?", Date.today).order(date_start: :asc).limit(1).pluck(:date_start).first }
   scope :get_ids_future_events, -> { where("DATE(date_start) > ?", Date.today).pluck(:id) }
-  scope :get_ids_date_event_old, -> { where("DATE(date_start) < ?", Date.today - 3.month).pluck(:id) }
+  scope :get_ids_date_event_old, -> { where("DATE(date_start) < ?", event_date_last).pluck(:id) }
 
 
   def self.get_collection_event_date(record)
-    date_end_before_next_generation = Date.today + 3.month
     array_records = []
 
     last_date_point = record.event.date_events.get_date_last_point
 
-    if  record.date_start < (date_end_before_next_generation)
+    if  record.date_start < event_date_future
       array_records << record.attributes.dup
     end
 
@@ -33,8 +32,8 @@ class DateEvent < ActiveRecord::Base
     new_date_start = template_record["date_start"]
     new_date_start = get_new_date_start(new_date_start, type_event)
 
-    if  new_date_start < (date_end_before_next_generation)
-      while new_date_start < (date_end_before_next_generation)
+    if  new_date_start < event_date_future
+      while new_date_start < event_date_future
 
         template_record["date_start"] = new_date_start
         template_record["id"] = nil
@@ -83,6 +82,14 @@ class DateEvent < ActiveRecord::Base
       when 'year_repeat'
         new_date_start + 1.years
     end
+  end
+
+  def event_date_last
+    Date.today - 3.month
+  end
+
+  def event_date_future
+    Date.today + 3.month
   end
 
 end
